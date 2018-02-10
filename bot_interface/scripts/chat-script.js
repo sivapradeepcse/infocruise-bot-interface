9d10020905e424c6752fccc4ba87f1561c4feb36
Sathesh Rgs <satheshrgs@gmail.com>
2018-02-10 19:24:40 +0530

    Changes

---
+++ bot_interface/scripts/chat-script.js
@@ -0,0 +1,5 @@
+//<![CDATA[
+function loadChatbox(){var e=document.getElementById("minim-chat");e.style.display="block";var e=document.getElementById("maxi-chat");e.style.display="none";var e=document.getElementById("chatbox");e.style.margin="0";}
+function closeChatbox(){var e=document.getElementById("chatbox");e.style.margin="0 0 -1500px 0";}
+function minimChatbox(){var e=document.getElementById("minim-chat");e.style.display="none";var e=document.getElementById("maxi-chat");e.style.display="block";var e=document.getElementById("chatbox");e.style.margin="0 0 -500px 0";}
+//]]>
