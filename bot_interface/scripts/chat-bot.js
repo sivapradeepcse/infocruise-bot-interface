9550898c2dc5ccb46ee572c7c27d71107bf12c24
Sathesh Rgs <satheshrgs@gmail.com>
2018-02-10 21:39:59 +0530

    changes

--- bot_interface/scripts/chat-bot.js
+++ bot_interface/scripts/chat-bot.js
@@ -2,7 +2,7 @@
 	var div1 = "<div class='chatbox' id='chatbox'>";
 		var div2="<div id='outer'>";
 			var div3="<div id='inner'>";
-				var div4="<iframe src='https://infocruise.herokuapp.com/customer' scrolling='no' frameborder='0' width='400px' height='530px' id='ifr'></iframe>";
+				var div4="<iframe src='link here' scrolling='no' frameborder='0' width='400px' height='530px' id='ifr'></iframe>";
 			var div5="</div>";
 		var div6="</div>";
 		var div7="<div id='minim-chat' onclick='minimChatbox()'><span class='minim-button'>&minus;</span></div>";
